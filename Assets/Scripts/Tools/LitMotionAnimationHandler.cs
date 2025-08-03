5110f0c1e82fbf036eba3689500e94f1e56dfe10
ilya be <belozertsev.i.24@outlook.com>
2025-08-03 14:42:03 +0200

    add litmotion animation handler

---
+++ Assets/Scripts/Tools/LitMotionAnimationHandler.cs
@@ -0,0 +1,20 @@
+﻿using Cysharp.Threading.Tasks;
+using LitMotion.Animation;
+using System;
+
+public static class LitMotionAnimationHandler
+{
+    public static async UniTask PlayAsync(LitMotionAnimation animation, Action onComplete = null)
+    {
+        if (animation == null)
+        {
+            onComplete?.Invoke();
+            return;
+        }
+
+        animation.Play();
+        while (animation.IsPlaying)
+            await UniTask.Yield();
+        onComplete?.Invoke();
+    }
+}
