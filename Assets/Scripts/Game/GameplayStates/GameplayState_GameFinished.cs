73e76b05478bee9c5fc2c1fcb5cfd4a8c0ad8429
ilya be <belozertsev.i.24@outlook.com>
2025-08-06 23:46:38 +0200

    fix naming

--- Assets/Scripts/Game/GameplayStates/GameplayState_GameFinished.cs
+++ Assets/Scripts/Game/GameplayStates/GameplayState_GameFinished.cs
@@ -1,19 +1,19 @@
 ﻿public abstract class GameplayState_GameFinished : IGameplayState
 {
     private readonly EventBus _eventBus;
-    private readonly IScoresService _coresService;
+    private readonly IScoresService _scoresService;
 
     public abstract bool IsWin { get; }
 
     public GameplayState_GameFinished(EventBus eventBus, IScoresService scoresService)
     {
         _eventBus = eventBus;
-        _coresService = scoresService;
+        _scoresService = scoresService;
     }
 
     public virtual void Enter()
     {
-        _eventBus.Publish(new GameEvent_GameFinished(IsWin, _coresService.GetCurrent()));
+        _eventBus.Publish(new GameEvent_GameFinished(IsWin, _scoresService.GetCurrent()));
     }
 
     public virtual void Exit()
